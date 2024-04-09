9c272cbfbbb7a74a88afb143a650e2e72719364a
Alain Greppin <agreppin@chilibi.ch>
2024-04-09 14:52:46 +0200

    also bind /run/act

--- setup-alpine.sh
+++ setup-alpine.sh
@@ -265,7 +265,9 @@ mkdir -p proc
 mount -v -t proc none proc
 mount_bind /dev dev
 mount_bind /sys sys
-mount_bind "$RUNNER_HOME/work" "${RUNNER_HOME#/}/work"
+mount_bind "$GITHUB_WORKSPACE" "${GITHUB_WORKSPACE#/}"
+[ -d /run/act ] &&
+mount_bind /run/act run/act
 
 # Some systems (Ubuntu?) symlinks /dev/shm to /run/shm.
 if [ -L /dev/shm ] && [ -d /run/shm ]; then
